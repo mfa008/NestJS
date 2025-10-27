c8b860236be73dd7d7c014f485ff61caee531078
mfa008 <fallmouha2005@gmail.com>
2025-10-27 11:49:09 +0000

    Ajout des fonctions CRUD dans le fichier position.service

--- src/position/position.module.ts
+++ src/position/position.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PositionController } from './position.controller';
 import { PositionService } from './position.service';
-
+import { PrismaService } from 'src/prisma.sevice';
 @Module({
   controllers: [PositionController],
-  providers: [PositionService]
+  providers: [PositionService, PrismaService],
 })
 export class PositionModule {}
--- src/position/position.service.ts
+++ src/position/position.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { PrismaService } from 'src/prisma.sevice';
 @Injectable()
-export class PositionService {}
+export class PositionService {
+  constructor(private readonly prisma: PrismaService) {}
+  // Retourner tous les positions
+  async getAllPosition() {
+    return await this.prisma.position.findMany();
+  }
+  async getOneposition(id: number) {
+    const position = await this.prisma.position.findUnique({
+      where: { id },
+    });
+    if (!position) {
+      throw new NotFoundError("Aucun position n'existe avec cet id ");
+    }
+    return await position;
+  }
+
+  async createPosition(data) {
+    const positionExist = await this.prisma.position.findFirst({
+      where: { name: data.name },
+    });
+
+    if (positionExist) {
+      throw new BadRequestException('Cette poste existe deja ');
+    }
+
+    return await this.prisma.position.create({ data });
+  }
+
+  async updatePosition(data, id: number) {
+    const position = await this.prisma.position.findUnique({
+      where: { id },
+    });
+    if (!position) {
+      throw new NotFoundError("Aucun position n'existe avec cet id ");
+    }
+    return await this.prisma.position.update({
+      where: { id },
+      data,
+    });
+  }
+  
+  async deletePosition(id:number) {
+    const positionExist = await this.prisma.position.findUnique({
+        where: {id}
+    });
+
+    if (!positionExist){
+        throw new NotFoundError("Position non trouve ");
+    }
+
+    return await this.prisma.position.delete({
+        where: {id}
+    });
+
+  }
+
+}
