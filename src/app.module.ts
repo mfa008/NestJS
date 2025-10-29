71883f674afeb93e6c4f1e9d557ee1b673a24b06
mfa008 <fallmouha2005@gmail.com>
2025-10-29 10:38:45 +0000

    Creation du model et des fichiers module, controller et service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { PositionModule } from './position/position.module';
 import { TeamModule } from './team/team.module';
 import { PlayerModule } from './player/player.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PositionModule, TeamModule, PlayerModule],
-  controllers: [AppController],
+  imports: [PositionModule, TeamModule, PlayerModule, AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
